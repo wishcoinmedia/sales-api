5041f0458b15a9e24ab6480452639cd14db694b7
Shreevari SP <shreevari.sp@protonmail.com>
2020-08-01 10:15:41 +0530

    Enabled CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object('wcm-sales.DevConfig')
 mongo = PyMongo(app)
 
